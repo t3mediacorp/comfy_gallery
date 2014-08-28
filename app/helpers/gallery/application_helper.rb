c639b39628f41621437435b53477721432d3731b
John Williams <jwilliams@t3media.com>
2014-08-28 14:16:28 -0400

    Updates for adjusting UI to look more like ActiveAdmin in bloomberg.

--- app/helpers/gallery/application_helper.rb
+++ app/helpers/gallery/application_helper.rb
@@ -2,7 +2,7 @@ module Gallery::ApplicationHelper
   
   def comfy_gallery_form_for(record_or_name_or_array, *args, &proc)
     options = args.extract_options!
-    form_for(
+    comfy_form_for(
       record_or_name_or_array,
       *(args << options.merge(:builder => ComfyGallery.config.form_builder.to_s.constantize)),
       &proc
